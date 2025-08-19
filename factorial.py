d3c00a0f8eb111818574a37e451625b7ad052603
Pramod Mane <pramod.mane703@gmail.com>
2025-08-19 11:44:25 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,15 @@
+
+
+def factorial(n):
+    if (n == 0 or n == 1):
+        return 1
+    
+    return n * factorial(n-1)
+
+def main():
+    num = int(input("Enter a number: "))
+    result = factorial(num)
+    print(f"Factorial of {num} is {result}")
+
+if __name__ == "__main__":
+    main()
---
+++ math.py
@@ -0,0 +1,11 @@
+import math
+
+num = int(input("Enter a number: "))
+
+sqrt = math.sqrt(num)
+log = math.log(num)
+sin = math.sin(num)
+
+print("Square root:",sqrt)
+print("Logarithm:",log)
+print("Sine:",sin)
